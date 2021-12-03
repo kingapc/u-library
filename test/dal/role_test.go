a52b837d2d9a0f3c3993dde578e56430a55bcad2
Reinaldo Pineda <Reinaldo.Pineda@focusservices.com>
2021-12-02 23:57:27 -0600

    Adding unit test

--- test/dal/role_test.go
+++ test/dal/role_test.go
@@ -1,35 +1,66 @@
 package dal
 
 import (
-	"reflect"
+	"context"
+	"database/sql"
+	"log"
 	"testing"
+	"time"
 
-	dal "github.com/rpinedafocus/u-library/internal/dal"
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/google/uuid"
 	"github.com/rpinedafocus/u-library/internal/model"
+	"github.com/stretchr/testify/assert"
 )
 
+var r = &model.RoleEntity{
+	Entity: model.Entity{
+		ID:        uuid.New().String(),
+		CreatedBy: "user",
+		CreatedAt: time.Now().Format("01-02-2006"),
+	},
+	Role: model.Role{
+		Name: "Test",
+	},
+}
+
+type repository struct {
+	db *sql.DB
+}
+
+func NewMock() (*sql.DB, sqlmock.Sqlmock) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		log.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+
+	return db, mock
+}
+
 func TestCreateRole(t *testing.T) {
-	type args struct {
-		role *model.Role
+	db, mock := NewMock()
+	repo := &repository{db}
+
+	defer func() {
+		repo.db.Close()
+	}()
+
+	query := "INSERT INTO university.roles \\(id, name, created_by, created_at\\) VALUES \\(\\$1, \\$2, \\$3, \\$4\\)"
+
+	prep := mock.ExpectPrepare(query)
+	prep.ExpectExec().WithArgs(r.ID, r.Name, r.CreatedBy, r.CreatedAt).WillReturnResult(sqlmock.NewResult(0, 1))
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	myQuery := "INSERT INTO university.roles (id, name, created_by, created_at) VALUES ($1, $2, $3, $4)"
+	stmt, err := repo.db.PrepareContext(ctx, myQuery)
+	if err != nil {
+		panic(err)
 	}
-	tests := []struct {
-		name    string
-		args    args
-		want    *model.RoleEntity
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := dal.CreateRole("root", tt.args.role)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("CreateRole() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("CreateRole() = %v, want %v", got, tt.want)
-			}
-		})
-	}
+	defer stmt.Close()
+
+	_, err = stmt.ExecContext(ctx, r.ID, r.Name, r.CreatedBy, r.CreatedAt)
+
+	assert.NoError(t, err)
 }
--- test/handler/book_test.go
+++ test/handler/book_test.go
@@ -2,104 +2,248 @@ package handler
 
 import (
 	"bytes"
+	"context"
+	"database/sql"
 	"encoding/json"
+	"log"
 	"net/http/httptest"
 	"testing"
+	"time"
 
+	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/gin-gonic/gin"
-	handler "github.com/rpinedafocus/u-library/internal/handler"
+	"github.com/google/uuid"
 	"github.com/rpinedafocus/u-library/internal/model"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestCreateBookController(t *testing.T) {
+type repository struct {
+	db *sql.DB
+}
+
+func NewMock() (*sql.DB, sqlmock.Sqlmock) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		log.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+
+	return db, mock
+}
+
+//Struct in BookController
+type MockBookStruct struct {
+	SessionDetailFn func(*gin.Context) *model.AccessDetails
+}
+
+//Session detail
+var e = &model.AccessDetails{
+	AccessUuid: "0b766bed69764a6a81c393c702aad3ed",
+	MyId:       "27f7f309d76e4ff3adf286cdf6712c1a",
+	UserId:     "admin",
+	RoleId:     "2",
+}
+
+//Session detail context
+var myData = map[string]interface{}{
+	"uuid":   e.AccessUuid,
+	"myId":   e.MyId,
+	"userId": e.UserId,
+	"roleId": e.RoleId,
+}
+
+var b = &model.FetchBook{
+	ID:             uuid.New().String(),
+	Title:          "Test Book",
+	Author:         "Author Test",
+	Genre:          "Genre Test",
+	PublishDate:    "01-01-1990",
+	TotalAvailable: 12,
+}
+
+func TestCreateBookControllerSession(t *testing.T) {
 
 	w := httptest.NewRecorder()
 	gin.SetMode(gin.TestMode)
 	c, _ := gin.CreateTestContext(w)
-
-	e := &model.AccessDetails{}
-	e.AccessUuid = "0b766bed69764a6a81c393c702aad3ed"
-	e.MyId = "27f7f309d76e4ff3adf286cdf6712c1a"
-	e.UserId = "admin"
-	e.RoleId = "2"
-
-	myData := map[string]interface{}{
-		"uuid":   e.AccessUuid,
-		"myId":   e.MyId,
-		"userId": e.UserId,
-		"roleId": e.RoleId,
-	}
-	var option int = 1 //change the switch option to see a fail test
 
 	c.Set("myData", myData)
 
 	b := &model.Book{}
 	b.Title = "Cien años de soledad III"
-	b.AuthorId = 8 //Set a fake AuthorId if you want to see an error
-	b.GenreId = 1  //Set a fake GenreId if you want to see an error
+	b.AuthorId = 8
+	b.GenreId = 1
 	b.PublishDate = "01-01-1967"
 	b.TotalAvailable = 1
 
 	body, _ := json.Marshal(b)
-	c.Request = httptest.NewRequest("POST", "http://localhost:8080/books/create", bytes.NewReader(body))
+	c.Request = httptest.NewRequest("POST", "/books/create", bytes.NewReader(body))
 
-	handler.CreateBookController(c)
+	myMock := MockBookStruct{}
+	myMock.SessionDetailFn = func(c *gin.Context) *model.AccessDetails {
+		data, _ := c.Get("myData")
+		mapData, _ := data.(map[string]interface{})
 
-	switch option {
-	case 1:
-		t.Run("Success", func(t *testing.T) { assert.Equal(t, 201, w.Code) })
-	case 2:
-		t.Run("Fail", func(t *testing.T) { assert.NotEqual(t, 201, w.Code) })
+		e := &model.AccessDetails{}
+		e.AccessUuid = mapData["uuid"].(string)
+		e.MyId = mapData["myId"].(string)
+		e.UserId = mapData["userId"].(string)
+		e.RoleId = mapData["roleId"].(string)
+
+		return e
 	}
+
+	assert.Equal(t, myMock.SessionDetailFn(c), e)
 }
 
-func TestFetchAllBooksController(t *testing.T) {
+func TestCreateBookControllerSessionFail(t *testing.T) {
 
 	w := httptest.NewRecorder()
 	gin.SetMode(gin.TestMode)
 	c, _ := gin.CreateTestContext(w)
 
-	var option int = 1 //change the switch option to see a fail test
+	c.Set("myData", myData)
 
-	c.Request = httptest.NewRequest("GET", "http://localhost:8080/books", nil)
-	c.Request.RequestURI = "http://localhost:8080/books"
-	c.Request.URL.Path = "http://localhost:8080/books"
+	b := &model.Book{}
+	b.Title = "Cien años de soledad III"
+	b.AuthorId = 8
+	b.GenreId = 1
+	b.PublishDate = "01-01-1967"
+	b.TotalAvailable = 1
 
-	handler.FetchAllBooksController(c)
+	body, _ := json.Marshal(b)
+	c.Request = httptest.NewRequest("POST", "/books/create", bytes.NewReader(body))
 
-	switch option {
-	case 1:
-		t.Run("Success", func(t *testing.T) { assert.Equal(t, 200, w.Code) })
-	case 2:
-		t.Run("Fail", func(t *testing.T) { assert.NotEqual(t, 200, w.Code) })
+	myMock := MockBookStruct{}
+	myMock.SessionDetailFn = func(c *gin.Context) *model.AccessDetails {
+		e := &model.AccessDetails{}
+		return e
+	}
+
+	assert.NotEqual(t, myMock.SessionDetailFn(c), e)
+}
+
+func TestFetchAllBooksController(t *testing.T) {
+
+	db, mock := NewMock()
+	repo := &repository{db}
+
+	defer func() {
+		repo.db.Close()
+	}()
+
+	query := `SELECT b.id, b.title, a.name author, g.name genre, b.publish_date, b.total_available 
+				FROM university.books b
+				INNER JOIN university.authors a on a.author_id = b.author_id
+				INNER JOIN university.genres g on g.genre_id = b.genre_id
+				WHERE b.active = true AND b.is_deleted = 'N'`
+
+	rows := sqlmock.NewRows([]string{"id", "title", "author", "genre", "publish_date", "total_avilable"}).
+		AddRow(b.ID, b.Title, b.Author, b.Genre, b.PublishDate, b.TotalAvailable)
+
+	mock.ExpectQuery(query).WillReturnRows(rows)
+	books := make([]*model.FetchBook, 0)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	myQuery := `SELECT b.id, b.title, a.name author, g.name genre, b.publish_date, b.total_available 
+				FROM university.books b
+				INNER JOIN university.authors a on a.author_id = b.author_id
+				INNER JOIN university.genres g on g.genre_id = b.genre_id
+				WHERE b.active = true AND b.is_deleted = 'N'`
+
+	rowsR, err := repo.db.QueryContext(ctx, myQuery)
+
+	for rowsR.Next() {
+		book := new(model.FetchBook)
+		err = rowsR.Scan(
+			&book.ID,
+			&book.Title,
+			&book.Author,
+			&book.Genre,
+			&book.PublishDate,
+			&book.TotalAvailable,
+		)
+
+		books = append(books, book)
+	}
+
+	assert.NotEmpty(t, books)
+	assert.NoError(t, err)
+	assert.Len(t, books, 1)
+}
+
+func TestFetchAllBooksControllerError(t *testing.T) {
+
+	db, mock := NewMock()
+	repo := &repository{db}
+
+	defer func() {
+		repo.db.Close()
+	}()
+
+	query := `SELECT b.id, b.title, a.name author, g.name genre, b.publish_date, b.total_available 
+				FROM university.books b
+				INNER JOIN university.authors a on a.author_id = b.author_id
+				INNER JOIN university.genres g on g.genre_id = b.genre_id
+				WHERE b.active = true AND b.is_deleted = 'N'`
+
+	rows := sqlmock.NewRows([]string{"id", "title", "author", "genre", "publish_date", "total_avilable"}).
+		AddRow(b.ID, b.Title, b.Author, b.Genre, b.PublishDate, b.TotalAvailable)
+
+	mock.ExpectQuery(query).WillReturnRows(rows)
+	books := make([]*model.FetchBook, 0)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	myQuery := `SELECT b.id, b.title, a.name author, g.name genre, b.publish_date, b.total_available 
+				FROM university.books b
+				INNER JOIN university.authors a on a.author_id = b.author_id
+				INNER JOIN university.genres g on g.genre_id = b.genre_id
+				WHERE b.active = true AND b.is_deleted = 'S'`
+
+	rowsR, err := repo.db.QueryContext(ctx, myQuery)
+
+	if rowsR == nil {
+		assert.Empty(t, books)
+		assert.Error(t, err)
+		assert.Len(t, books, 0)
 	}
 }
 
 func TestFetchBookByIdController(t *testing.T) {
 
-	w := httptest.NewRecorder()
-	gin.SetMode(gin.TestMode)
-	c, _ := gin.CreateTestContext(w)
+	db, mock := NewMock()
+	repo := &repository{db}
 
-	var params = map[string]string{"id": "0b766bed69764a6a81c393c702aad3ed"}
-	var paramsSlice []gin.Param
-	var option int = 1 //change the switch option to see a fail test
+	defer func() {
+		repo.db.Close()
+	}()
 
-	for key, value := range params {
-		paramsSlice = append(paramsSlice, gin.Param{
-			Key:   key,
-			Value: value,
-		})
-	}
-	c.Params = paramsSlice
+	query := "SELECT b.id, b.title, a.name author, g.name genre, b.publish_date, b.total_available " +
+		"FROM university.books b " +
+		"INNER JOIN university.authors a on a.author_id = b.author_id " +
+		"INNER JOIN university.genres g on g.genre_id = b.genre_id " +
+		"WHERE b.active = true AND b.is_deleted = 'N' AND b.id = \\$1"
 
-	handler.FetchBookByIdController(c)
+	rows := sqlmock.NewRows([]string{"id", "title", "author", "genre", "publish_date", "total_avilable"}).
+		AddRow(b.ID, b.Title, b.Author, b.Genre, b.PublishDate, b.TotalAvailable)
 
-	switch option {
-	case 1:
-		t.Run("Success", func(t *testing.T) { assert.Equal(t, 200, w.Code) })
-	case 2:
-		t.Run("Fail", func(t *testing.T) { assert.NotEqual(t, 200, w.Code) })
-	}
+	mock.ExpectQuery(query).WithArgs(b.ID).WillReturnRows(rows)
+
+	book := new(model.FetchBook)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	myQuery := "SELECT b.id, b.title, a.name author, g.name genre, b.publish_date, b.total_available " +
+		"FROM university.books b " +
+		"INNER JOIN university.authors a on a.author_id = b.author_id " +
+		"INNER JOIN university.genres g on g.genre_id = b.genre_id " +
+		"WHERE b.active = true AND b.is_deleted = 'N' AND b.id = $1 "
+
+	err := repo.db.QueryRowContext(ctx, myQuery, b.ID).Scan(&book.ID, &book.Title, &book.Author, &book.Genre, &book.PublishDate, &book.TotalAvailable)
+
+	assert.NotNil(t, book)
+	assert.NoError(t, err)
 }
